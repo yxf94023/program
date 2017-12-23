fc70be390a994b5c1b0cf61e61c9333f4777c934
yxf94023 <1594149156@qq.com>
2017-12-23 23:08:31 +0800

    tijiao

--- unix_apue/code/c05_std_IO/c05_stdio.cc
+++ unix_apue/code/c05_std_IO/c05_stdio.cc
@@ -6,13 +6,22 @@
 
 #include <stdio.h>
 #include <wchar.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 /**
  *\brief 测试setbuf函数
  *
- * void setbuf(FILE *restrict fp, char *restrict buf);<br/>
- * void setvbuf(FILE *restrict fp, char *restrict buf, int mode, size_t size);<br/>
- *	返回值： 若成功则返回0， 若出错则返回非0值<br/>
+ *<code>
+ *void setbuf(FILE *restrict fp, char *restrict buf);
+ *
+ *void setvbuf(FILE *restrict fp, char *restrict buf, int mode, size_t size);
+ *
+ *返回值： 若成功则返回0， 若出错则返回非0值
+ *</code>
  *
  *对任何一个给定的流，如果我们并不喜欢这些系统默认的情况，则可调用上面2个函数进行缓冲类型修改<br/>
  *可以使用setbuf函数打开或关闭缓冲机制，为了带缓冲进行I/O，参数buf必须指向一个长度为BUFSIZ的缓冲区，通常在此之后该流就是全缓冲的，但是如果该流与一个终端设备相关，那么某些系统也可将其设置为行缓冲。为了关闭缓冲，将buf设置为NULL<br/>
@@ -21,8 +30,8 @@
  *<caption>setbuf和setvbuf函数</caption>
  *<tr><th>函数</th><th>mode</th><th>buf</th><th>缓冲区及长度</th><th>缓冲类型</th></tr>
  *<tr><td rowspan="2">setbuf</td><td rowspan="2"></td><td>非空</td><td>长度为BUFSIZ的用户buf</td><td>全缓冲或行缓冲</td></tr>
- *<tr><td>NULL</td><td>（五缓冲区）</td><td>不带缓冲</td></tr>
- *<tr><td rowspan="5">setbuf</td><td rowspan="2">_IOFBF</td><td>非空</td><td>长度为size的用户buf</td><td rowspan="2">全缓冲</td></tr>
+ *<tr><td>NULL</td><td>（无缓冲区）</td><td>不带缓冲</td></tr>
+ *<tr><td rowspan="5">setvbuf</td><td rowspan="2">_IOFBF</td><td>非空</td><td>长度为size的用户buf</td><td rowspan="2">全缓冲</td></tr>
  *<tr><td>NULL</td><td>合适长度的系统缓冲区</td></tr>
  *<tr><td rowspan="2">_IOLBF</td><td>非空</td><td>长度为size的用户buf</td><td rowspan="2">行缓冲</td></tr>
  *<tr><td>NULL</td><td>合适长度的系统缓冲区</td></tr>
@@ -61,15 +70,65 @@
  */
 int test_setbuf(int type)
 {
+	int i = 0;
+	int size = 10;
+	int ret = 0;
+	const int BUF_SIZE = 1024;
+	char buf[BUF_SIZE];
+	
+	ret = setvbuf(stdin, buf, type, sizeof(buf) -1);
+	if (ret){
+		
+		printf("%s:%d setvbuf failed(%s)\n", __FILE__, __LINE__, strerror(errno));
+		return -1;
+	}
+	for (i = 0; i < size; ++i){
+		
+		printf("%d ", i);
+	}
+	sleep(5);
+	
 	return 0;
+}
+
+static int convert_iobuf_type(const char *szbuftype)
+{
+	int size = 0, i = 0;
+	int buf_mode = _IOFBF;
+	
+	struct mode_pair{
+		
+		const char *str_buf;
+		int	buf_mode;
+	}mode_pairs[] = {
+		
+		{"_IOFBF", _IOFBF}, {"_IOLBF", _IOLBF}, {"_IONBF", _IONBF}
+	};
+	
+	size = sizeof(mode_pairs)/sizeof(mode_pairs[0]);
+	
+	for (i = 0; i < size; ++i){
+		
+		if (strstr(szbuftype, mode_pairs[i].str_buf)){
+			
+			buf_mode = mode_pairs[i].buf_mode;
+			break;
+		}
+	}
+	
+	printf("%s:%d size[%d], buf_mode[%d]\n", __FILE__, __LINE__, size, buf_mode);
+	return buf_mode;
 }
 
 /**
  *\brief 测试fflush函数
  *
- *int fflush(FILE *fp);<br/>
- *返回值：若成功则返回0， 若出错则返回EOF<br/>
- *<br/>
+ *<code>
+ *int fflush(FILE *fp);
+ *
+ *返回值：若成功则返回0， 若出错则返回EOF
+ *</code>
+ *
  *任何时候都可以强制冲洗一个流，函数使该流所有未写的数据都被传送到内核，作为一个特例，如若fp是NULL，则此函数将导致所有输出流被冲洗<br/>
  *\param[in] fp 	文件指针
  *\retval	0	成功
@@ -83,10 +142,15 @@ int test_fflush(FILE *fp)
 /**
  *\brief 测试gets函数
  *
- *char *fgets(char *restrict buf, int n, FILE *restrict fp);<br/>
- *char *gets(char *buf);<br/>
- *两个函数返回值：若成功则返回buf，若已到达文件结尾出错则返回NULL<br/>
- *<br/>
+ *<code>
+ *char *fgets(char *restrict buf, int n, FILE *restrict fp);
+ *
+ *char *gets(char *buf);
+ *
+ *两个函数返回值：若成功则返回buf，若已到达文件结尾出错则返回NULL
+ *</code>
+ *
+ *
  *1.两个函数都是每次输入一行
  *2.gets从标准输入读，fgets则从指定的流读
  *3.fgets需指定缓冲区的长度n，函数一直读到下一个换行符为止，但是不超过n-1个字符，读入的字符被送入缓冲区，缓冲区总是以null字符结尾
@@ -102,10 +166,13 @@ int test_gets()
 /**
  *\brief 测试puts函数
  *
- *int fputs(const char *restrict str, FILE *restrict fp);<br/>
- *int puts(const char *str);<br/>
- *返回值：若成功则返回非负值， 若出错则返回EOF<br/>
- *<br/>
+ *<code>
+ *int fputs(const char *restrict str, FILE *restrict fp);
+ *
+ *int puts(const char *str);
+ *
+ *返回值：若成功则返回非负值， 若出错则返回EOF
+ *</code>
  *
  *1.函数fputs将一个以null符种植的字符串写到指定流，尾端的终止符null不写出，函数并不一定是每次输出一行，因为它并不要求在null字符前一定是换行符。
  *2.puts将一个null符终止的字符串写到标准输出， 终止符不写出，但是， puts然后又将一个换行符写到标准输出
@@ -121,10 +188,14 @@ int test_puts()
 /**
  *\brief 测试fread/fwrite函数
  *
- *size_t fread(void *restrict ptr, size_t size, size_t nobj, FILE *restrict fp);<br/>
- *size_t fwrite(const void *restrict ptr, size_t size, size_t nobj, FILE *restrict fp);<br/>
+ *<code>
+ *size_t fread(void *restrict ptr, size_t size, size_t nobj, FILE *restrict fp);
+ *
+ *size_t fwrite(const void *restrict ptr, size_t size, size_t nobj, FILE *restrict fp);
+ *
  *两个函数的返回值：读或写的对象数
- *<br/><br/>
+ *</code>
+ *
  *使用场景
  *<ol>
  *<li>读或写一个二进制数组</li>
@@ -147,8 +218,11 @@ int test_fread_fwrite()
 /**
  *\brief 测试ftell函数
  *
- *long ftell(FILE *fp);<br/>
+ *<code>
+ *long ftell(FILE *fp);
+ *
  *返回值：成功返回当前文件位置指示， 若出错则返回-1L
+ *</code>
  *
  *\retval 0 	成功
  *\retval !0	失败
@@ -161,8 +235,11 @@ int test_ftell()
 /**
  *\brief 测试fseek函数
  *
- *int fseek(FILE *fp, long offset, int whence);<br/>
+ *<code>
+ *int fseek(FILE *fp, long offset, int whence);
+ *
  *返回值：若成功则返回0， 若出错则返回非0值
+ *</code>
  *
  *whence的取值
  *1.SEEK_SET 表示从文件的起始位置开始<br/>
@@ -180,9 +257,13 @@ int test_fseek()
 /**
  *\brief 测试fgetpos函数
  *
- *int fgetpos(FILE *restrict fp, fpos_t *restrict pos);<br/>
- *itn fsetpos(FILE *fp, const fpos_t *pos);<br/>
- *返回值：若成功则返回0， 若出错则返回非0值<br/>
+ *<code>
+ *int fgetpos(FILE *restrict fp, fpos_t *restrict pos);
+ *
+ *itn fsetpos(FILE *fp, const fpos_t *pos);
+ *
+ *返回值：若成功则返回0， 若出错则返回非0值
+ *</code>
  *
  *fgetspos将文件位置指示器的当前值存入由pos指向的对象中，在以后调用fsetpos时，可以使用此值将流重新定位到该位置。
  *
@@ -197,13 +278,21 @@ int test_fgetpos()
 /**
  *\brief 测试tmpfile/tmpname函数
  *
- *char *tmpnam(char *ptr);<br/>
- *char *tempnam(const char *directory, const char *prefix);<br/>
- *返回值: 指向唯一路径名的指针<br/>
- *FILE *tmpfile(void);<br/>
- *返回值: 若成功则返回文件指针， 若出错则返回NULL<br/>
- *int mkstemp(char *tmplate);<br/>
+ *<code>
+ *char *tmpnam(char *ptr);
+ *
+ *char *tempnam(const char *directory, const char *prefix);
+ *
+ *返回值: 指向唯一路径名的指针
+ *
+ *FILE *tmpfile(void);
+ *
+ *返回值: 若成功则返回文件指针， 若出错则返回NULL
+ *
+ *int mkstemp(char *tmplate);
+ *
  *返回值：若成功则返回文件描述符， 若出错则返回-1
+ *</code>
  *
  *<ol>
  *<li>tmpname产生一个与现有文件名不同的一个有效路径名字符串， 每次调用它时，都会产生一个不同的路径名，最多调用次数是TMP_MAX</li>
@@ -227,6 +316,24 @@ int test_fgetpos()
  */
 int test_tmpfile_tmpname()
 {
+	return 0;
+}
+
+static void show_help()
+{
+	printf("setbuf type, 设置输出缓冲区的类型\n"
+			"\ttype  _IOFBF|_IOLBF|_IONBF  \n\n");
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 3 && strcmp(argv[1], "setbuf") == 0){
+		
+		test_setbuf(convert_iobuf_type(argv[2]));
+	}else{
+		
+		show_help();
+	}
 	return 0;
 }
 
